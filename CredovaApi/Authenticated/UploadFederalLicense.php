db797c1e332b457d06788917b7049ea8efbbd209
Joshua fedoryszyn <josh.fedoryszyn@classyllama.com>
2019-06-03 23:01:54 -0500

    CRD-634_18 - getPath, getHeaders, and getResponse overridden to allow file uploads

--- CredovaApi/Authenticated/UploadFederalLicense.php
+++ CredovaApi/Authenticated/UploadFederalLicense.php
@@ -16,6 +16,8 @@ class UploadFederalLicense extends \ClassyLlama\Credova\CredovaApi\Authenticated
 
     const PATH = 'v2/federallicense/%s/uploadfile';
 
+    protected $file_loc = null;
+
     /**
      * @return string
      * @throws CredovaApiException
@@ -26,61 +28,96 @@ class UploadFederalLicense extends \ClassyLlama\Credova\CredovaApi\Authenticated
 //            throw new CredovaApiException(__('License number not set before license retrieval.'));
         }
 
-        return sprintf(static::PATH,'9512885139');
+        return sprintf(static::PATH, 'bb84a40a-dfc0-428c-924f-e60cd76a211e');
     }
+
+    public function setFileLoc($fileLoc)
+    {
+            $this->file_loc = $fileLoc;
+    }
+
+    protected function getFileLength(){
+        return $this->file_loc;
+    }
+
 
     /**
      * Get request method
      *
      * @return string
      */
-    protected function getMethod(): string
+    protected
+    function getMethod(): string
     {
         return \Zend\Http\Request::METHOD_POST;
     }
+
     /**
-     * Make request and get response
+     * Add authentication header
+     *
+     * {@inheritdoc}
+     * @throws \ClassyLlama\Credova\Exception\CredovaApiException
+     */
+    protected function getHeaders($contentLength = null) : array
+    {
+        $headers = parent::getHeaders();
+
+        $authToken = $this->apiHelper->getAuthToken();
+
+        $headers['Authorization'] = "Bearer $authToken";
+        $headers['Content-Type'] = "multipart/form-data";
+        if($contentLength){
+            $headers['Content-Length'] = $contentLength;
+
+        }
+        print_r($contentLength . "CONTENT");
+
+        return $headers;
+    }
+
+    protected function getContentLength(){
+        return sizeof($this->getData());
+    }
+
+    /**
+     * Make request and get response, Overridden from base to add custom headers and
+     * allow for file uploads
      *
      * @return \Zend\Http\Response
      * @throws \ClassyLlama\Credova\Exception\CredovaApiException
      */
-    public function getResponse() : \Zend\Http\Response
+    public
+    function getResponse(): \Zend\Http\Response
     {
+
+
         // Set log prefix which can be used to correlate request/response pairs
         // even if there are unrelated requests intermingled.
         $this->logPrefix = uniqid();
 
         /** @var \Zend\Http\Client $client */
-        $client = $this->clientFactory->create();
+        $client = $this->clientFactory->create(['timeout'=> 30]);
 
         $client->setUri($this->getUri());
         $client->setMethod($this->getMethod());
-        $client->setHeaders($this->getHeaders());
+
+
+
 
         if (!empty($this->getData())) {
-            $requestBody = json_encode($this->getData());
-
-            $this->debugLog($requestBody);
-
-            $client->setRawBody($requestBody);
+            $requestBody = ($this->getData());
+            print_r($requestBody);
+            $client->setFileUpload($requestBody['file'], 'file');
         }
+        $client->setHeaders($this->getHeaders(filesize($requestBody['file'])));
 
         $this->prepareRequest($client);
-
         $this->debugLog($client->getRequest()->toString());
-
         /** @var \Zend\Http\Response $response */
         $response = $client->send();
-
-        $this->debugLog(
-            $response->getStatusCode() . "\n" .
-            $response->getHeaders()->toString() . "\n" .
-            $response->getBody()
-        );
-
         if (!$response->isSuccess()) {
             var_dump($client);
-            throw new CredovaApiException(__('Same old same old'));
+            throw new CredovaApiException(__('Error on Credova API request'));
         }
 
         return $response;
